4f4ea5b8c76f35e70368fef0e932f1630788a64a
at0mbxmb <at0mbxmb@gmail.com>
2014-05-24 10:49:12 -0700

    read json data from file and parse out the good stuff

---
+++ parse_info_from_json.py
@@ -0,0 +1,28 @@
+import json
+from pprint import pprint
+import re
+
+json_data=open('crashlacma.20140524-102001.json')
+
+data = json.load(json_data)
+
+# TODO: handle test cases
+# testcases:
+# hollywood & vine, hollywood and vine
+# order of operations: hashtag, img, address, other text.
+# hashtag allcaps or lowercase
+# uploaded image, link to hosted image
+
+# if image is uploaded via twitter
+img_url = data["entities"]["media"][0]["media_url"]
+hashtag = data["entities"]["hashtags"][0]["text"]    # won't need this parsed
+tweet = data["text"]
+tweet_text = ' '.join(re.sub("(@[A-Za-z0-9]+)|(#[A-Za-z0-9]+)|(\w+:\/\/\S+)"," ",tweet).split())
+
+print(hashtag)
+print(img_url)
+print(tweet)
+print(tweet_text)
+
+
+json_data.close()
