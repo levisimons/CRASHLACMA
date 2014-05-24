54278aae202b07b3747e36a5b997cbe6bd733748
at0mbxmb <at0mbxmb@gmail.com>
2014-05-24 11:23:17 -0700

    added handling for images sent as url and images uploaded through twitter

--- parse_info_from_json.py
+++ parse_info_from_json.py
@@ -2,7 +2,8 @@ import json
 from pprint import pprint
 import re
 
-json_data=open('crashlacma.20140524-102001.json')
+#json_data=open('sample_http_img.json')
+json_data=open('sample_upload_img.json')
 
 data = json.load(json_data)
 
@@ -12,18 +13,27 @@ data = json.load(json_data)
 # order of operations: hashtag, img, address, other text.
 # hashtag allcaps or lowercase
 # uploaded image, link to hosted image
+# multiple urls? currently hard-coded to only accept the first url seen. probably best this way.
 
-# if image is uploaded via twitter
-img_url = data["entities"]["media"][0]["media_url"]
-hashtag = data["entities"]["hashtags"][0]["text"]    # won't need this parsed
+#pprint(data)
+
 tweet = data["text"]
+print tweet
+
+if data["entities"].get('media'): # img uploaded via twitter
+	###print "img uploaded"
+	img_url = data["entities"]["media"][0]["media_url"]	
+else: # img passed as url 
+	###print "img as url"
+	img_url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', tweet)[0]
+		
+hashtag = data["entities"]["hashtags"][0]["text"] # won't need this parsed
+
 # this regex scrapes out URLs. that'll need to change to handle imgs as hosted links
 tweet_text = ' '.join(re.sub("(@[A-Za-z0-9]+)|(#[A-Za-z0-9]+)|(\w+:\/\/\S+)"," ",tweet).split())
 
 print(hashtag)
 print(img_url)
-print(tweet)
 print(tweet_text)
-
 
 json_data.close()
