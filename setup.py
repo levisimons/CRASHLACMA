79701366c55d2a4161652f5eacb2488b956da5b3
at0mbxmb <at0mbxmb@gmail.com>
2014-05-31 20:04:36 -0700

    trying a setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+    name = "CRASHLACMA",
+    packages = ["geopy", "json", "pprint", "re", "urllib", "time", "Image", "os" ],
+    version = "1.0.2",
+    description = "Twitter Image Mapper",
+    author = "Michelle Leonhart",
+    author_email = "michelle@tinwhiskers.net",
+    url = "http://tinwhiskers.net/CRASHLACMA/map_display.php",
+    download_url = "https://github.com/levisimons/CRASHLACMA",
+    keywords = ["twitter", "art", "maps", "lacma", "crashspace"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        ],
+    long_description = """\
