5b34a86d255231861b4082d43254d8c188ea717e
at0mbxmb <at0mbxmb@gmail.com>
2014-05-31 20:09:24 -0700

    moving files to new dirs

---
+++ CRASHLACMA/grab_raw_tweets.py
@@ -0,0 +1,28 @@
+from slistener import SListener
+import time, tweepy, sys
+
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler('FE01Tcf6sDZpLG0vabqA', 'zuqcRA0XIoMU0o6swx6vzWENtWZNIAXHQtZWbfk')
+auth.set_access_token('31018035-FbvB97V4QDqoAcQAg49z1UYlgrssenP4L8cgzWah6', 'jPqA3vQkAvrjV2XUVIfjFBgHJWpNgvubrfsR6E9EJTuCH')
+api = tweepy.API(auth)
+
+DIR_RAW_TWEET_DATA='data_raw_tweets'
+
+output_file_prefix = 'crashlacma'
+
+def main():
+    track = ['#CRASHLACMA']
+ 
+    listen = SListener(api, DIR_RAW_TWEET_DATA + '/' + output_file_prefix)
+    stream = tweepy.Stream(auth, listen)
+
+    print "Streaming started..."
+
+    try: 
+        stream.filter(track = track)
+    except:
+        print "error!"
+        stream.disconnect()
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_twitter_json_parser.py
@@ -0,0 +1,20 @@
+from twitter_json_parser import TwitterJsonParser
+from nose.tools import assert_equals
+from mock import Mock
+
+mock = Mock()
+parser = TwitterJsonParser()
+
+mock.get_data_from_tweets('data_raw_tweets/sample_mixed_data.json')
+
+def test_get_data_from_tweets():
+	assert_equals(str(parser.get_data_from_tweets('data_raw_tweets/sample_mixed_data.json')), 
+	"[u'Hollywood and Vine #CRASHLACMA http://t.co/1yr7rB7rFJ', u'Hollywood and Vine', " \
+	"u'http://pbs.twimg.com/media/BobePLhCEAA2qoI.jpg', u'Bagley and Venice #CRASHLACMA " \
+	"http://t.co/ioPBJgPDtB', u'Bagley and Venice', u'http://pbs.twimg.com/media/BobeYT_CcAA1wFc.jpg', " \
+	"u'#CRASHLACMA Motor and Venice 90034 http://t.co/BCyyL7zjIN', u'Motor and Venice 90034', " \
+	"u'http://pbs.twimg.com/media/BobaovcIcAIeRNz.jpg', u'Culver and Washington #CRASHLACMA " \
+	"https://t.co/3XNSeXyqgI', u'Culver and Washington', u'https://t.co/3XNSeXyqgI', " \
+	"u'#CRASHLACMA Overland and Venice http://t.co/sfgHxU5h6h', u'Overland and Venice', " \
+	"u'http://t.co/sfgHxU5h6h', u'#CRASHLACMA Sunset and Alverado http://t.co/CK4abafurB', " \
+	"u'Sunset and Alverado', u'http://pbs.twimg.com/media/BoasuVvIIAAP9CS.jpg']")
